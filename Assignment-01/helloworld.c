b93a23726d280adb6792180033cda929941df1e3
Nguyễn Phước Thành Lâm <phuocthanhlamnguyen@gmail.com>
2026-07-15 11:59:59 +0700

    Turning high-level to low-level
    Replaced printf with low-level write for output.

--- Assignment-01/helloworld.c
+++ Assignment-01/helloworld.c
@@ -1,7 +1,14 @@
-#include <stdio.h>
+#include <unistd.h> // For write()
 
-int main(void)
+#define X 15 // Make an char limit
+
+typedef struct Char { // Other way that's different from cpp class
+char var1[X]; //The variables
+}Char; 
+
+int main(void/*No Command line algument*/)
 {
-  printf("Hello, World!\n");
-  return 0;
+  Char writing = {"Hello, World!\n"}; //To say Hello, World!\n 
+  write(1, writing.var1, sizeof(writing.var1)); // low-level way to say write
+  return 0; // Function is over
 }
